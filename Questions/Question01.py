17b17c65ce336c6d714bab9c17150b4b626958c5
L00163287 <L00163287@student.lyit.ie>
2021-11-01 09:51:29 +0000

    Initial commit for OOP

---
+++ Questions/Question01.py
@@ -0,0 +1,12 @@
+"""
+#
+# @File         : Question01.py
+# @Created      : 2021-10-25 11:50
+# @Author       : Bubashankushan B
+# @Version      : v1.0.0
+# @Licensing    : 
+#
+# @Description  :
+#
+"""
+
---
+++ Questions/__init__.py
@@ -0,0 +1,11 @@
+"""
+#
+# @File         : __init__.py.py
+# @Created      : 2021-11-01 09:50
+# @Author       : Bubashankushan B
+# @Version      : v1.0.0
+# @Licensing    : 
+#
+# @Description  :
+#
+"""
